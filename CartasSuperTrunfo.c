48940167147c9e6fd6181a77e999da3947f3c344
sergioarthurtds <teixeirasilva737@gmail.com>
2025-04-24 22:55:46 +0000

    Nível Novato

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -10,8 +10,6 @@ int main()
     int pturisticos1;
     float area1;
     float pib1;
-    float densidadepop1; // Variavel para Densidade Populacional (população / área)
-    float pibpercapta1;  // Variavel para PIB per Capta (pib / população)
 
     // Dados da Carta 2
     char estado2;
@@ -21,8 +19,6 @@ int main()
     int pturisticos2;
     float area2;
     float pib2;
-    float densidadepop2; // Variavel para Densidade Populacional (população / área)
-    float pibpercapta2;  // Variavel para  PIB per Capta (pib / população)
 
     // Entrada de dados da primeira carta
     printf("Carta 1:\n");
@@ -72,41 +68,22 @@ int main()
     printf("Número de Pontos Turísticos: ");
     scanf("%d", &pturisticos2);
 
-    // Exibição dos dados Carta 1
+    // Exibição dos dados
     printf("\nCarta 1 \n");
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigo1);
     printf("Nome da Cidade: %s\n", cidade1);
-    printf("População: %d\n", populacao1);
+    printf("População: %d Habitantes\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f Bilhões de reais\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pturisticos1);
 
-    // Calcula a Densidade Populacional Carta 1
-    densidadepop1 = populacao1 / area1;
-    printf("Densidade Populacional: %.2f hab/km²\n", densidadepop1);
-
-    // Calcula o PIB per Capta Carta 1
-    pibpercapta1 = (pib1 * 1000000000) / populacao1;
-    printf("PIB per Capta: %.2f reais\n", pibpercapta1);
-
-    // Exibição dos dados Carta 2
     printf("\nCarta 2\n");
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
     printf("Nome da Cidade: %s\n", cidade2);
-    printf("População: %d\n", populacao2);
+    printf("População: %d Habitantes\n", populacao2);
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f Bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pturisticos2);
-
-    // Calcula a Densidade Populacional Carta 2
-    densidadepop2 = populacao2 / area2;
-    printf("Densidade Populacional: %.2f hab/km²\n", densidadepop2);
-
-    // Calcula o PIB per Capta Carta 1
-    pibpercapta2 = (pib2 * 1000000000) / populacao2;
-    printf("PIB per Capta: %.2f reais\n", pibpercapta2);
-
-    return 0;
 }
